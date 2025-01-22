09e8cc0661c8cf2f0ea3e3a349cab4ff562d040a
Valentin Raille <valrail78@gmail.com>
2025-01-22 15:36:02 +0100

    lint

--- weather.py
+++ weather.py
@@ -1,9 +1,8 @@
 # pylint: disable=missing-module-docstring
 
 import sys
-import urllib.parse
-import requests
 from datetime import datetime
+import requests
 
 BASE_URI = "https://weather.lewagon.com"
 
@@ -31,7 +30,7 @@ def search_city(query):
     if len(city_list) > 1:
         for index, value in enumerate(city_list):
             print(f"{index + 1}. {value['name']},{value['country']}")
-        city_index = input(f"Multiple matches found, which city did you mean?\n> ")
+        city_index = input("Multiple matches found, which city did you mean?\n> ")
         city = city_list[int(city_index) - 1]
     else:
         city = city_list[city_index]
@@ -42,7 +41,8 @@ def weather_forecast(lat, lon):
     weather_forecast_list = []
     url = f"{BASE_URI}/data/2.5/forecast?lat={lat}&lon={lon}&units=metric"
     response = requests.get(url).json()
-    five_days_forecast_list = [response.get('list', "no list key in forecast response")[i] for i in [4,12,20,28,36]] # get the weather at 12:00pm
+    # get the weather at 12:00pm
+    five_days_forecast_list = [response.get('list', "unfound list key")[i] for i in [4,12,20,28,36]]
     for forecast in five_days_forecast_list:
         weather_forecast_list.append(create_forecast_dict(forecast))
     return weather_forecast_list
@@ -50,7 +50,10 @@ def weather_forecast(lat, lon):
 def create_forecast_dict(raw_dict):
     '''Return the dictionary in right format from a timestamp dictionnary'''
     day = datetime.fromtimestamp(raw_dict['dt']).strftime('%Y-%m-%d')
-    return {'day' : day, 'weather' : f"{raw_dict['weather'][0]['description'].title()} {raw_dict['main']['temp']}°C"}
+    return {
+        'day' : day,
+        'weather' : f"{raw_dict['weather'][0]['description'].title()} {raw_dict['main']['temp']}°C"
+    }
 
 def main():
     '''Ask user for a city and display weather forecast'''
